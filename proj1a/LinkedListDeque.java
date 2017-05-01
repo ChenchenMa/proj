b828fe14fd3439013e9955fd02bd0edfb4db2f8f
ChenchenMa <machenchen0131@gmail.com>
2017-05-02 00:54:18 +0800

    Add files via upload

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,10 +1,10 @@
-public class LinkedListDeque <Item> {
+public class LinkedListDeque<Item> {
 
     private class Node {
         public Item item;
         public Node next;
         public Node before;
-        public Node (Item item, Node next, Node before){
+        Node(Item item, Node next, Node before) {
             this.item = item;
             this.next = next;
             this.before = before;
@@ -14,77 +14,92 @@ public class LinkedListDeque <Item> {
     private Node Sentinal;
     private int size;
 
-    public LinkedListDeque(){
-    size = 0;
-    Sentinal = new Node(null,null,null);
-    Sentinal.next=Sentinal;
-    Sentinal.before=Sentinal;
+    public LinkedListDeque() {
+        size = 0;
+        Sentinal = new Node(null, null, null);
+        Sentinal.next = Sentinal;
+        Sentinal.before = Sentinal;
     }
 
     
-    public void addFirst(Item item){
-        size+=1;
+    public void addFirst(Item item) {
+        size += 1;
         Sentinal.next = new Node(item, Sentinal.next, Sentinal);
-        Sentinal.next.next.before=Sentinal.next;
+        Sentinal.next.next.before = Sentinal.next;
     }
 
-    public void addLast(Item item){
-        Sentinal.before.next=new Node(item, Sentinal, Sentinal.before);
-        Sentinal.before=Sentinal.before.next;
-        size+=1;
+    public void addLast(Item item) {
+        Sentinal.before.next = new Node(item, Sentinal, Sentinal.before);
+        Sentinal.before = Sentinal.before.next;
+        size += 1;
     }
 
-    public boolean isEmpty(){
-        if(size==0) return true;
-        else return false;
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public void printDeque(){
+    public void printDeque() {
         Node currentNode = Sentinal.next;
-        while(currentNode.next!=Sentinal){
+        while (currentNode.next != Sentinal) {
             System.out.print(currentNode.item + " ");
             currentNode = currentNode.next;
         }
     }
 
-    public Item removeFirst(){
+    public Item removeFirst() {
         Node first = Sentinal.next;
-        if(first==null) return null;
+        if (first == null) {
+            return null;
+        }
         Sentinal.next = Sentinal.next.next;
         Sentinal.next.before = Sentinal;
         size -= 1;
         return first.item;
     }
 
-    public Item removeLast(){
+    public Item removeLast() {
         Node last = Sentinal.before;
-        if(last==null) return null;
+        if (last == null) {
+            return null;
+        }
         Sentinal.before = Sentinal.before.before;
-        Sentinal.before.next=Sentinal;
-        size -=1;
+        Sentinal.before.next = Sentinal;
+        size -= 1;
         return last.item;
     }
 
-    public Item get(int index){
-        if(index+1>size) return null;
+    public Item get(int index) {
+        if (index + 1 > size) {
+            return null;
+        }
         Node currentNode = Sentinal.next;
-        for(int i=0; i<index;i++){
-        currentNode = currentNode.next;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
         }
         return currentNode.item;
     }
 
-    private Item getRecursiveHelper(Node front, int index){
-        if(front == null) return null;
-        if(index == 0) return front.item;
-        else return getRecursiveHelper(front.next, index-1);
+    private Item getRecursiveHelper(Node front, int index) {
+        if (front == null) {
+            return null;
+        }
+        if (index == 0) {
+            return front.item;
+        } else {
+            return getRecursiveHelper(front.next, index - 1);
+        }
+
     }
 
-    public Item getRecursive(int index){
+    public Item getRecursive(int index) {
         return getRecursiveHelper(Sentinal.next, index);
     }
 
