91b584c64d5cf4e629a11fd7f4b6fdaec0e2d07a
ChenchenMa <machenchen0131@gmail.com>
2017-05-02 00:18:15 +0800

    Add files via upload

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -7,50 +7,51 @@ public class ArrayDeque <Item> {
     private int size;
     private int nextLast;
     private int nextFirst;
-    private int length;
 
     public ArrayDeque(){
         items = (Item[]) new Object[8];
         size = 0;
-        nextFirst = 3;
-        nextLast = 4;
-        length=8;
+        nextFirst = 0;
+        nextLast = 1;
     }
 
     private void resize(int capacity, int start){
         Item[] a = (Item[]) new Object[capacity];
-        System.arraycopy(items, 0, a, start, size);
-        items = a;
-        length = capacity;
+        if(nextLast<nextFirst) {
+            System.arraycopy(items, 0, a, start, size);
+        }
+        else{
+            System.arraycopy(items, nextLast, a, start,size-nextLast);
+            System.arraycopy(items, 0, a, start+size-nextLast, nextFirst+1);
+        }
+            items = a;
+            nextLast=start+size;
+            nextFirst=start-1;
     }
 
     public void addFirst(Item item){
         if(size == items.length){
             resize (size*3, size);
-            nextFirst = size-1;
-            nextLast = size*2+1;
         }
         items[nextFirst] = item;
         if(nextFirst!=0){nextFirst-=1;}
-        else{nextFirst = length-1;}
+        else{nextFirst = items.length-1;}
         size+=1;
     }
 
     public void addLast(Item item){
         if(size == items.length){
             resize (size*3, size);
-            nextFirst = size-1;
-            nextLast = size*2+1;
         }
         items[nextLast]=item;
-        if(nextLast!=length-1){nextLast+=1;}
+        if(nextLast!=items.length-1){nextLast+=1;}
         else{nextLast = 0;}
         size+=1;
     }
 
     public boolean isEmpty(){
-        if(size==0) return true;
-        else return false;
+        if(size==0) {return true;}
+        else {return false;}
     }
 
     public int size(){
@@ -58,33 +59,37 @@ public class ArrayDeque <Item> {
     }
 
     public void printDeque(){
-        if(nextFirst<nextLast){
-            for(int i = 1; i<=size; i++){
-                System.out.print(items[nextFirst+i]+" ");
+        if(nextLast-nextFirst+1 == size){
+            for(int i = 0; i<size; i++){
+                System.out.print(items[nextFirst+i+1]+" ");
             }
         }
-        else{
-            for(int i = nextFirst+1; i<length; i++){
+        else if(nextFirst<nextLast){
+            for(int i = nextFirst+1; i<items.length; i++){
                 System.out.print(items[i]+" ");
             }
             for(int i = 0; i<nextLast; i++){
                 System.out.print(items[i]+" ");
             }
         }
+        else{
+            for(int i=0; i<size;i++)
+                System.out.print(items[i]+"");
+        }
     }
 
     public Item removeFirst(){
-        if(size*4<length){
+        if(size*4<items.length){
             resize(size*3, size);
             nextFirst = size-1;
-            nextLast = size*2+1;
+            nextLast = size*2;
         }
        Item first = (Item) new Object();
-        if(nextFirst!=length-1){
+        if(nextFirst!=items.length-1){
             first = items[nextFirst+1];
             nextFirst+=1;
         }
-        if(nextFirst == length-1){
+        if(nextFirst == items.length-1){
             first = items[0];
             nextFirst = 0;
         }
@@ -93,10 +98,10 @@ public class ArrayDeque <Item> {
     }
 
     public Item removeLast(){
-        if(size*4<length){
+        if(size*4<items.length){
             resize(size*3, size);
             nextFirst = size-1;
-            nextLast = size*2+1;
+            nextLast = size*2;
         }
         Item last = (Item) new Object();
         if(nextLast != 0){
@@ -104,8 +109,8 @@ public class ArrayDeque <Item> {
             nextLast = nextLast-1;
         }
         if(nextLast == 0){
-            last = items[length-1];
-            nextLast = length-1;
+            last = items[items.length-1];
+            nextLast = items.length-1;
         }
         size-=1;
         return last;
@@ -113,7 +118,12 @@ public class ArrayDeque <Item> {
 
     public Item get(int index){
         if(index>size) return null;
-        else return items[nextFirst+1+index];
+        else if(nextFirst+1+index<items.length) {
+            return items[nextFirst + 1 + index];
+        }
+        else{
+            return items[index-items.length+1+nextFirst];
+        }
     }
 
 }
