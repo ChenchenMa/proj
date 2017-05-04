eead0ec36abc210bc272d927e26e534e0786ecba
ChenchenMa <machenchen0131@gmail.com>
2017-05-05 02:32:47 +0800

    Create AbstractBoundedDeque.java

---
+++ hw1/synthesizer/AbstractBoundedDeque.java
@@ -0,0 +1,23 @@
+package synthesizer;
+import synthesizer.BoundedQueue;
+
+/**
+ * Created by Administrator on 2017/5/5.
+ */
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T>{
+    protected int fillCount;
+    protected int capacity;
+    public int capacity(){
+        return capacity;
+    }
+    public int fillCount(){
+        return fillCount;
+    }
+    public boolean isEmpty(){
+        return fillCount == 0;
+    }
+    public boolean isFull(){
+        return fillCount == capacity;
+    }
+
+}
