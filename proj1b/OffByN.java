b695bc5744cff56273ae45888f008f0f1bd5bf12
ChenchenMa <machenchen0131@gmail.com>
2017-05-04 21:20:55 +0800

    Create OffByN.java

---
+++ proj1b/OffByN.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2017/5/4.
+ */
+public class OffByN implements CharacterComparator{
+    int n;
+
+    public OffByN( int n){
+        this.n = n;
+    }
+
+    @Override
+    public boolean equalChars(char x, char y){
+        return (x-y == n || y-x == n);
+    }
+
+}
